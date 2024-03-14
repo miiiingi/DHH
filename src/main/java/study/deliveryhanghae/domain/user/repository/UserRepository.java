8be4e6ae474d067c8a15271d96bea97d7a73d706
miiiingi <kimmingi722@gmail.com>
2024-03-14 11:46:01 +0900

    feat: 회원가입 기능 추가

--- src/main/java/study/deliveryhanghae/domain/user/repository/UserRepository.java
+++ src/main/java/study/deliveryhanghae/domain/user/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package study.deliveryhanghae.domain.user.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.deliveryhanghae.domain.user.entity.User;
 
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String username);
+
+    Optional<User> findByNickname(String nickname);
 }
+
