d5944ebe516f2b4c8de9f1fbfeb53e5b4c8201dd
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-03-15 13:14:51 +0900

    Feature/5 mainpage (#9)
    * feat: 전체 업장 조회 기능 추가
    
    * feat: 업장 조회 front 추가
    
    * feat: index.html 업장 클릭시 상세 페이지 링크 추가
    
    * fix: index.html 잘못된 css 링크 수정
    
    ---------
    
    Co-authored-by: ramizzang <rkfkaals@gmail.com>

--- src/main/java/study/deliveryhanghae/domain/store/entity/Store.java
+++ src/main/java/study/deliveryhanghae/domain/store/entity/Store.java
@@ -1,4 +1,36 @@
 package study.deliveryhanghae.domain.store.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "store")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    private List<Menu> menuList = new ArrayList<>();
+
+    @Builder
+    public Store(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
 }
--- src/main/java/study/deliveryhanghae/domain/store/service/StoreService.java
+++ src/main/java/study/deliveryhanghae/domain/store/service/StoreService.java
@@ -1,5 +1,35 @@
 package study.deliveryhanghae.domain.store.service;
 
-public class StoreService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import study.deliveryhanghae.domain.store.dto.StoreResponseDto.StoreListDto;
+import study.deliveryhanghae.domain.store.repository.StoreRepository;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+    private final StoreRepository storeRepository;
+
+    // 업장 전체 목록 조회
+    public List<StoreListDto> getStoreList() {
+        List<StoreListDto> storeList = storeRepository.findAll()
+                .stream()
+                .map(store -> new StoreListDto(store.getId(),
+                        store.getName(),
+                        store.getImageUrl()))
+                .toList();
+        return storeList;
+    }
+
+    public List<StoreListDto> getSearchStroeList(String searchMenu) {
+        List<StoreListDto> storeList = storeRepository.findByMenuListNameContaining(searchMenu)
+                .stream()
+                .map(store -> new StoreListDto(store.getId(),
+                        store.getName(),
+                        store.getImageUrl()))
+                .toList();
+        return storeList;
+    }
 }
