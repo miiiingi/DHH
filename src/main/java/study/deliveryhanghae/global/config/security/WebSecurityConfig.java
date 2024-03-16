64db78e5776f2dc0d9d10b2acd662863dcf02964
miiiingi <kimmingi722@gmail.com>
2024-03-16 16:52:11 +0900

    refactor: formlogin 코드 삭제

--- src/main/java/study/deliveryhanghae/global/config/security/WebSecurityConfig.java
+++ src/main/java/study/deliveryhanghae/global/config/security/WebSecurityConfig.java
@@ -84,19 +84,12 @@ public class WebSecurityConfig {
         );
 
         http.authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests
-                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                .requestMatchers("/**").permitAll()
-                                .requestMatchers("/orders/").hasAuthority("ROLE_USER")
-                                .anyRequest().authenticated()
+                authorizeHttpRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/orders/").hasAuthority("ROLE_USER")
+                        .anyRequest().authenticated()
         );
-
-//        http.formLogin((formLogin) ->
-//                formLogin.loginProcessingUrl("/login-page")
-//                        .permitAll()
-//                        .successHandler(customAuthenticationSuccessHandler())
-//                        .failureHandler(customAuthenticationFailureHandler())
-//        );
 
         http.exceptionHandling((e) -> e.accessDeniedHandler(customAccessDeniedHandler));
         http.exceptionHandling((e) -> e.authenticationEntryPoint(customAuthenticationEntryPoint));
