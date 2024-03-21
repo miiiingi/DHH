6330e6dda6c5d2f853beb04c12c63cace9f47279
ramizzang <rkfkaals@gmail.com>
2024-03-21 12:27:00 +0900

    feat : 사장님 회원가입, 로그인 추가

--- src/main/java/study/deliveryhanghae/domain/owner/controller/OwnerController.java
+++ src/main/java/study/deliveryhanghae/domain/owner/controller/OwnerController.java
@@ -1,25 +1,64 @@
 package study.deliveryhanghae.domain.owner.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import study.deliveryhanghae.domain.order.dto.OrderResponseDto.getOrderDto;
 import study.deliveryhanghae.domain.order.service.OrderService;
+import study.deliveryhanghae.domain.owner.dto.OwnerRequestDto;
 import study.deliveryhanghae.domain.owner.service.OwnerService;
+import study.deliveryhanghae.global.handler.exception.BusinessException;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/v2")
+@RequiredArgsConstructor
 public class OwnerController {
     private final OrderService orderService;
     private final OwnerService ownerService;
 
-    public OwnerController(OrderService orderService, OwnerService ownerService) {
-        this.orderService = orderService;
-        this.ownerService = ownerService;
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    public void returnNoFavicon() {
     }
+
+    @GetMapping("/v2/signup")
+    public String signupPage(Model model) {
+        model.addAttribute("ErrorCode", null);
+        model.addAttribute("ErrorMessage", null);
+        return "v2Signup";
+    }
+
+    @GetMapping("/v2/login-page")
+    public String loginPage() {
+        return "v2Login";
+    }
+
+    @Operation(summary = "회원가입", description = "회원 가입시 필요한 정보를 입력합니다.")
+    @PostMapping("/v2/signup")
+    public String signup(OwnerRequestDto.SignupRequestDto requestDto, Model model) {
+        try {
+            ownerService.signUp(requestDto);
+        } catch (BusinessException ex) {
+            model.addAttribute("ErrorCode", ex.getErrorCode().getStatus());
+            model.addAttribute("ErrorMessage", ex.getErrorCode().getMessage());
+            return "v2Signup";
+        }
+        return "redirect:/v2/login-page";
+    }
+
+
+    /***
+     *  사장님 메인 페이지 이동
+     * @param model
+     * @return
+     */
     @GetMapping
     public String getOwnerMain(Model model){
         List<getOrderDto> orderList=orderService.getOrderList();
--- src/main/java/study/deliveryhanghae/domain/owner/dto/OwnerRequestDto.java
+++ src/main/java/study/deliveryhanghae/domain/owner/dto/OwnerRequestDto.java
@@ -1,4 +1,37 @@
 package study.deliveryhanghae.domain.owner.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class OwnerRequestDto {
+
+    public record SignupRequestDto(
+            @NotNull(message = "이메일 필수")
+            @Email(message = "올바른 이메일 형식을 입력해주세요.")
+            @Schema(description = "이메일", example = "test@test.com")
+            String email,
+            @Size(min = 8, max = 15, message = "비밀번호 8자 이상, 15자 이하로 입력해야 합니다.")
+            @Schema(description = "비밀번호", example = "12345678a1!")
+            String password,
+
+            @Schema(description = "이름", example = "김르탄")
+            String name
+
+    ) {
+    }
+
+    public record LoginRequestDto(
+            @NotNull(message = "이메일 필수")
+            @Email(message = "올바른 이메일 형식을 입력해주세요.")
+            @Schema(description = "이메일", example = "test@test.com")
+            String email,
+            @Size(min = 8, max = 15, message = "비밀번호 8자 이상, 15자 이하로 입력해야 합니다.")
+            @Schema(description = "비밀번호", example = "12345678a1!")
+            String password
+
+    ) {
+    }
+
 }
