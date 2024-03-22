58244861077ce991fc3fd0ff975db1bacf3d894d
weiß Yi <78130010+Rosa-Eau@users.noreply.github.com>
2024-03-22 20:40:13 +0900

    Feature/54 (#57)
    * feat: 이미지 파일 확장자 검사 추가
    
    * refactor: 로그 삭제

--- src/main/java/study/deliveryhanghae/global/config/security/jwt/JwtAuthorizationFilter.java
+++ src/main/java/study/deliveryhanghae/global/config/security/jwt/JwtAuthorizationFilter.java
@@ -37,7 +37,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         if (StringUtils.hasText(tokenValue)) {
             // JWT 토큰 substring
             tokenValue = jwtUtil.substringToken(tokenValue);
-            log.info(tokenValue);
+            log.info("tokenValue : " + tokenValue);
 
             if (!jwtUtil.validateToken(tokenValue)) {
                 log.error("Token Error");
