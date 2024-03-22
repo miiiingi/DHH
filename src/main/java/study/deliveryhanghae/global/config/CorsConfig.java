ec6992fe0f95b5a7e754be3f228abcb8681e26d0
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-03-23 03:55:53 +0900

    refactor: CorsConfig 위치 수정

--- src/main/java/study/deliveryhanghae/global/config/CorsConfig.java
+++ src/main/java/study/deliveryhanghae/global/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package study.deliveryhanghae.global.config.security;
+package study.deliveryhanghae.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
