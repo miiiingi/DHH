e8cce3c39047189bd3dd5e7280e1deac81cfd530
uha9218 <77494780+uha9218@users.noreply.github.com>
2024-03-24 16:02:58 +0900

    feature: timer 기능 추가 (#74)

---
+++ src/main/java/study/deliveryhanghae/global/aop/Timer.java
@@ -0,0 +1,11 @@
+package study.deliveryhanghae.global.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Timer {}
